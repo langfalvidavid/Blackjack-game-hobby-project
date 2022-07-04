732bdff7587d78f13428f2648a795b5759d6b9ec
langfalvidavid <langfalvi.david@gmail.com>
2022-07-04 19:35:05 +0200

    tiny changes

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 
-const cardsDP = document.getElementById("cards")
-const sumDP = document.getElementById("sum")
-const messageDP = document.getElementById("message")
-const newCardBtn = document.getElementById("new-card-btn")
-const restartGameBtn = document.getElementById("restart-game-btn")
+const cardsDP = document.getElementById("cards") //div that cards are generated into
+const sumDP = document.getElementById("sum") //Sum of cards
+const messageDP = document.getElementById("message") //Message that is visible
+const newCardBtn = document.getElementById("new-card-btn") //Button that randomly generates a new card
+const restartGameBtn = document.getElementById("restart-game-btn") //Button that restarts game
+const balance = document.getElementById("balance") //Player's balance
 const cards=["2_of_clubs-min.jpg","2_of_diamonds-min.jpg","2_of_hearts-min.jpg","2_of_spades-min.jpg",
             "3_of_clubs-min.jpg","3_of_diamonds-min.jpg","3_of_hearts-min.jpg","3_of_spades-min.jpg",
             "4_of_clubs-min.jpg","4_of_diamonds-min.jpg","4_of_hearts-min.jpg","4_of_spades-min.jpg",
@@ -17,14 +18,14 @@ const cards=["2_of_clubs-min.jpg","2_of_diamonds-min.jpg","2_of_hearts-min.jpg",
             "jack_of_clubs2-min.jpg","jack_of_diamonds2-min.jpg","jack_of_hearts2-min.jpg","jack_of_spades2-min.jpg",
             "king_of_clubs2-min.jpg","king_of_diamonds2-min.jpg","king_of_hearts2-min.jpg","king_of_spades2-min.jpg",
             "queen_of_clubs2-min.jpg","queen_of_diamonds2-min.jpg","queen_of_hearts2-min.jpg","queen_of_spades2-min.jpg"]  
-
+//random card generator
 function randomCard(){
     return cards[Math.floor(Math.random()*cards.length)]
     
 }
-let newCard=randomCard()
+let newCard=randomCard() //variable for the generated card (i.e. "2_of_clubs-min.jpg")
 
-
+//function that gives the random card value
 function cardValue(){
     if(newCard[0]==="2" || newCard[0]==="3" || newCard[0]==="4" || newCard[0]==="5" || newCard[0]==="6" 
         || newCard[0]==="7" || newCard[0]==="8" || newCard[0]==="9"  )
@@ -50,54 +51,64 @@ function cardValue(){
 
 
 
-let sumVal=0
-let count=0
+let sumVal=0 //sum of cards
  newCardBtn.addEventListener("click",function() {
     if(inGame===true){
-    count++
-    let value = cardValue()
-    const img = document.createElement("img");
+    let value = cardValue() //value of generated card
+    //generating new elements for each card
+    const img = document.createElement("img") 
     img.id="img"
-    img.src ="cards/"+newCard
+    img.src ="cards/"+newCard //file path
     img.style.maxWidth="3rem"
     img.style.margin="0 5px "
-    document.getElementById("cards").appendChild(img)
+    img.style.borderRadius="10px"
+    img.style.animation.slice(0,1)
+    document.getElementById("cards").appendChild(img) //pushing newly generated card into its place
     sumVal+=value
-    sumDP.textContent=`Sum: ${sumVal}`
-    gameLogic()
-    newCard=randomCard()
+    sumDP.textContent=`Sum: ${sumVal}`//displaying current sum
+    gameLogic() // decides whether player has lost or still in game
+    newCard=randomCard() // pulling new card from deck
 }
 })
 
-let inGame = true
+let inGame = true //if false, game is over
 let message = 
 ["Do you want to draw a new card? 😉",
 "Congrats! You've got blackjack! 🥳",
 "Sorry, you're out of the game! 😭"]
-
+let win = false
 function gameLogic(){
-    messageDP.style.display="block"
-    if(sumVal<=21){messageDP.textContent=message[0]}
-    else if(sumVal===21){messageDP.textContent=message[1]}
-    else{
+        if(sumVal<=21){messageDP.textContent=message[0]}
+        else if(sumVal===21){messageDP.textContent=message[1]}
+        else{
         messageDP.textContent=message[2]
         inGame=false
     }
 }
 
+//resetting game to its default stage
 restartGameBtn.addEventListener("click",function(){
-    if(inGame===false){
         messageDP.textContent="Draw a card to start the game! 😀"
         sumDP.textContent="Sum: 0"
         sumVal=0
         let deleteCards = document.getElementById("cards")
+        //counting how many elements does the cards div have, then deleting them
         let childrenX = deleteCards.childElementCount
         for(let i=0;i<childrenX;i++){
             let del = document.getElementById("img")
             del.remove()
-        }
-
-
         inGame=true
     }
 })
+
+// ---- Dealer system ----
+
+function dealer(){
+
+}
+
+
+//---- Balance system ----
+function balanceSystem(){
+
+}
